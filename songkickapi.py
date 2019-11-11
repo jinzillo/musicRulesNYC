13175a0cf80794be6857cd5b30886d6a69dac02a
sgoldfar <56848186+sgoldfar@users.noreply.github.com>
2019-11-11 14:47:20 -0500

    Add files via upload

---
+++ songkickapi.py
@@ -0,0 +1,24 @@
+
+import requests
+import json 
+
+# r = requests.get('https://api.songkick.com/api/3.0/search/locations.json?query=Brooklyn&apikey=m9DM1BFnj2MsPG3R')
+r = requests.get('https://api.songkick.com/api/3.0/metro_areas/7644/calendar.json?apikey=m9DM1BFnj2MsPG3R')
+#requesting info for metropolitan area 
+
+print(r.status_code)
+
+print(r.text)
+
+data = json.loads(r.text)
+
+print(data)
+
+
+
+
+
+
+
+#requesting artist history:
+ # https://api.songkick.com/api/3.0/artists/{artist_id}/gigography.json?apikey={m9DM1BFnj2MsPG3R}
