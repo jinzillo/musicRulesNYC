cd3226db200dab666b71145da85a9d55150671bc
jay <jinzillo@pratt.edu>
2019-12-06 22:15:38 -0500

    edited comments

--- hiphop_eventsListAPI.py
+++ hiphop_eventsListAPI.py
@@ -9,13 +9,13 @@ while loop_counter < 4:
     loop_counter = loop_counter + 1 ### move invrementation in front of first call to avoid requesting page zero
     print "loop-counter: ",  loop_counter ### debug tracking of this variable
 
-    events = api.call('/events/search', q='tag: hiphop', date='2019110100-2019113000', category='concerts', location='New York City', within=10, page_count=20, page_number=loop_counter)
+    events = api.call('/events/search', q='tag: hiphop', date='2017110100-2017113000', category='concerts', location='New York City', within=10, page_count=20, page_number=loop_counter)
      
     
     all_events = all_events + events['events']['event'] ### this works and takes data out of list 
     # all_events.append(events) ### list.append() method allows me to append the entire elements dictionary to the all_events list
     # all_events.append('-----===========<*************>===========-----') ### marker so I can see where each record starts in the printout
-    # print (all_events) 
+    print (all_events) 
 
-with open('hiphop_events_NovemberNYC.json', 'w') as outfile:
-	json.dump(all_events, outfile, indent=2)
+# with open('hiphop_events_NovemberNYC.json', 'w') as outfile:
+# 	json.dump(all_events, outfile, indent=2)
--- rock_eventsListAPI.py
+++ rock_eventsListAPI.py
@@ -3,14 +3,14 @@ import eventful, json
 loop_counter = 0
 all_events = []
 
-api = eventful.API('95HzcLgHb9mKLh6P') 
+api = eventful.API('95HzcLgHb9mKLh6P')  
 
 while loop_counter < 26:  
-    loop_counter = loop_counter + 1 ### move invrementation in front of first call to avoid requesting page zero
+    loop_counter = loop_counter + 1 ### 
     print "loop-counter: ",  loop_counter ### debug tracking of this variable
 
     events = api.call('/events/search', q='tag: rock', date='2019110100-2019113000', category='concerts', location='New York City', within=10, page_count=20, page_number=loop_counter)
-     
+    ## query, date and location can be changed for various searches
     
     all_events = all_events + events['events']['event'] ### this works and takes data out of list 
     # all_events.append(events) ### list.append() method allows me to append the entire elements dictionary to the all_events list
